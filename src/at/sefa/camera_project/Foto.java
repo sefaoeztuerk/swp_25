899af518a6923e5b3a3fde8bf9ac9efc0b356983
Sefa Öztürk <sefa.oeztuerk@student.htldornbirn.at>
2024-10-24 10:55:52 +0200

    Refactor Foto class: use 'this' keyword and enhance getInfo

--- src/at/sefa/camera_project/Foto.java
+++ src/at/sefa/camera_project/Foto.java
@@ -1,8 +1,29 @@
 package at.sefa.camera_project;
 
-
 public class Foto {
     private String name;
     private String date;
     private float size;
+
+    public Foto(String name, String date, float size) {
+        this.name = name;
+        this.date = date;
+        this.size = size;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public float getSize() {
+        return this.size;
+    }
+
+    public String getInfo() {
+        return "Picture: " + this.name + ", Date: " + this.date + ", Size: " + this.size + " MB";
+    }
 }

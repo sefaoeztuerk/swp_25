4ca3bbc589c9378e0a04a8696458a9de5182d707
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-11-07 10:58:37 +0100

    Add SDCard class to manage photo storage with capacity check

--- src/at/sefa/camera_project/SDCard.java
+++ src/at/sefa/camera_project/SDCard.java
@@ -4,5 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SDCard {
+    private int capacity; 
+    private List<Foto> photos; 
+    private float usedSpace; 
 
+    public SDCard(int capacity) {
+        this.capacity = capacity;
+        this.photos = new ArrayList<>();
+        this.usedSpace = 0;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public float getAvailableSpace() {
+        return capacity - usedSpace;
+    }
+
+    public void savePhoto(Foto foto) {
+        if (usedSpace + foto.getSize() <= capacity) {
+            photos.add(foto);
+            usedSpace += foto.getSize(); 
+        } else {
+            System.out.println("Error: Not enough space to save the photo.");
+        }
+    }
+
+    public List<Foto> getPhotos() {
+        return photos; 
+    }
 }
